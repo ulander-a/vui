7c5b0a631b312aa60cd775e3e5d21f509e1bec22
Alve Ulander <alve.ulander@xtractor.se>
2019-06-07 12:46:39 -0100

    fetch some demo data

--- src/components/Api.vue
+++ src/components/Api.vue
@@ -1,19 +1,26 @@
 <template>
-  <h2>
-    Currently connected to
-    <span>{{ api.name }}</span>
-  </h2>
+  <div>
+    <h2>
+      Current API:&nbsp;
+      <span>{{ api.name }}</span>
+    </h2>
+    <DataDisplay />
+  </div>
 </template>
 
 <script>
+import DataDisplay from "@/components/DataDisplay.vue";
 export default {
   name: "Api",
   data() {
     return {
       api: {
-          name: process.env.VUE_APP_API_NAME
+        name: process.env.VUE_APP_API_NAME
       }
-    }
+    };
+  },
+  components: {
+      DataDisplay
   }
-}
+};
 </script>
---
+++ src/components/DataDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="data-display">
+    <p>{{ toDisplay }}</p>
+  </div>
+</template>
+
+<script>
+const url = "https://swapi.co/api/people/1/";
+fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json));
+
+
+export default {
+  name: "DataDisplay",
+  data() {
+      return {
+          toDisplay: 'Display here'
+      }
+  }
+};
+</script>
