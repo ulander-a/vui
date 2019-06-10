34ef1f3dbc19c7ee317ac046be1c625b2c43739e
Alve Ulander <alve.ulander@xtractor.se>
2019-06-10 13:06:06 -0100

    display data on mount

--- src/components/DataDisplay.vue
+++ src/components/DataDisplay.vue
@@ -5,18 +5,36 @@
 </template>
 
 <script>
-const url = "https://swapi.co/api/people/1/";
-fetch(url)
-    .then(response => response.json())
-    .then(json => console.log(json));
+const url = "https://swapi.co/api/people/1/"
+// fetch(url)
+//   .then(response => response.json())
+//   .then(json =>  console.log(json))
 
 
 export default {
   name: "DataDisplay",
+  methods: {
+    getData(url) {
+      this.isFetching = true
+      fetch(url)
+        .then(res => res.json())
+        .then(json => {
+          this.isFetching = false
+          this.toDisplay = json
+          console.log(json)
+        })
+        .catch(err => this.isFetching = false)
+    }
+  },
   data() {
       return {
+          isFetching: false,
           toDisplay: 'Display here'
       }
+  },
+  mounted: function() {
+    console.log('Mounted')
+    this.getData(url)
   }
-};
+}
 </script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About'
+import Data from './views/Data.vue'
 
 Vue.use(Router)
 
@@ -17,12 +19,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     },
     {
       path: '/data',
       name: 'data',
-      component: () => import('./views/Data.vue')
+      component: Data
     }
   ]
 })
