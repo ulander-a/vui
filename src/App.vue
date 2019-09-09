737d278bf6d463838577ca1d89f4c2adccafeb5d
Alve Ulander <alve.ulander@xtractor.se>
2019-09-09 10:59:56 -0100

    re-implement routing

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,48 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
-        <v-list-item @click="">
+        <v-list-item :to="{ path: '/' }">
           <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
+            <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
+            <v-list-item-title>Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item @click="">
+        <v-list-item :to="{ path: '/about' }">
           <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
+            <v-icon>mdi-information</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :to="{ path: '/api' }">
+          <v-list-item-action>
+            <v-icon>mdi-api</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Data</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      clipped-left
-    >
+    <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>XConsole</v-toolbar-title>
       <div class="flex-grow-1"></div>
-      <v-btn 
-        icon
-        v-on:click="toggleTheme"
-      >
+      <v-btn icon v-on:click="toggleTheme">
         <v-icon>mdi-theme-light-dark</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <!-- <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/bXNzZL"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
-        </v-row> -->
+      <v-container class="fill-height" fluid>
+        
+        <router-view />
+      
       </v-container>
     </v-content>
 
@@ -90,22 +53,22 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: null,
-    }),
-    created () {
-      this.$vuetify.theme.dark = true
-    },
-    methods: {
-      toggleTheme() {
-        this.$vuetify.theme.dark ? 
-        this.$vuetify.theme.dark = false :
-        this.$vuetify.theme.dark = true
-      }
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark
+        ? (this.$vuetify.theme.dark = false)
+        : (this.$vuetify.theme.dark = true);
     }
   }
+};
 </script>
